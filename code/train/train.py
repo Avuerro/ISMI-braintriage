030b4c2b6c728f91abd897116bdccb2d81c7a36b
fbergh <freekbergh@gmail.com>
2020-06-02 17:05:23 +0200

    Added conversion to GPU and formatting

--- code/train/train.py
+++ code/train/train.py
@@ -6,8 +6,9 @@ import wandb
 
 class Trainer(object):
     
-    def __init__(self,model,criterion, optimizer,  train_loader, val_loader, n_epochs,model_dir):
-        self.model = model
+    def __init__(self,model,criterion, optimizer,  train_loader, val_loader, n_epochs, model_dir, device):
+        self.device = device
+        self.model = model.to(device)
         self.optimizer = optimizer
         self.criterion = criterion
         self.train_loader = train_loader
@@ -27,8 +28,8 @@ class Trainer(object):
             self.model.train()
             self.optimizer.zero_grad()
 
-            images = images.float()
-            targets = targets.float()
+            images = images.float().to(self.device)
+            targets = targets.float().to(self.device)
 
             predictions = self.model(images)
             loss = self.criterion(predictions, targets)
@@ -37,11 +38,10 @@ class Trainer(object):
             self.optimizer.step()
 
             #Accuracy
-            accuracy_predictions = (torch.sigmoid(predictions)>0.5).float()
-
+            accuracy_predictions = (torch.sigmoid(predictions.detach().cpu())>0.5).float()
             correct = (accuracy_predictions == targets).float().sum()/accuracy_predictions.shape[0]
 
-            self.loss_history['training'][-1] += float(loss.data)
+            self.loss_history['training'][-1] += float(loss.detach().cpu().data)
             self.acc_history['training'][-1] += float(correct)
 
         self.loss_history['training'][-1] /= batch_idx + 1
@@ -54,17 +54,17 @@ class Trainer(object):
         for batch_idx, (images, targets) in tqdm(enumerate(self.val_loader), total=len(self.val_loader), desc="#test_batches", leave=False):
             self.model.eval()
 
-            images = images.float()
-            targets = targets.float()
+            images = images.float().to(self.device)
+            targets = targets.float().to(self.device)
 
             predictions = self.model(images)
             loss = self.criterion(predictions, targets)
 
             #Accuracy
-            accuracy_predictions = (torch.sigmoid(predictions)>0.5).float()
+            accuracy_predictions = (torch.sigmoid(predictions.detach().cpu())>0.5).float()
             correct = (accuracy_predictions == targets).float().sum()/accuracy_predictions.shape[0]
 
-            loss_history['validation'][-1] += float(loss.data)
+            loss_history['validation'][-1] += float(loss.detach().cpu().data)
             acc_history['validation'][-1] += float(correct)
 
         loss_history['validation'][-1] /= batch_idx + 1
@@ -73,8 +73,6 @@ class Trainer(object):
 
     def train_and_validate(self):
         print(f'Running {self.model.name}')
-
-
         best_val_loss = 9999999
 
         for epoch in tqdm(range(self.n_epochs), desc="#epochs"):
@@ -86,7 +84,11 @@ class Trainer(object):
                 best_val_loss = self.loss_history['validation'][-1]
                 torch.save(self.model.state_dict(), '{:s}/{:s}_{:03d}.npz'.format(self.model_dir, self.model.name, epoch))
 
-            wandb.log({"Training Loss":self.loss_history['training'][-1],"Training Accuracy":self.acc_history['training'][-1],"Validation Loss":self.loss_history['validation'][-1],"Validation Accuracy":self.acc_history['validation'][-1]})
+            wandb.log({"Training Loss":self.loss_history['training'][-1],
+                       "Training Accuracy":self.acc_history['training'][-1],
+                       "Validation Loss":self.loss_history['validation'][-1],
+                       "Validation Accuracy":self.acc_history['validation'][-1]})
 
-            print('epoch: {:3d} / {:03d}, training loss: {:.4f}, validation loss: {:.4f}, training accuracy: {:.3f}, validation accuracy: {:.3f}.'.format(epoch + 1, self.n_epochs, self.loss_history['training'][-1], self.loss_history['validation'][-1], self.acc_history['training'][-1], self.acc_history['validation'][-1]))
+            print('epoch: {:3d} / {:03d}, training loss: {:.4f}, validation loss: {:.4f}, training accuracy: {:.3f}, validation accuracy: {:.3f}.'\
+                .format(epoch + 1, self.n_epochs, self.loss_history['training'][-1], self.loss_history['validation'][-1], self.acc_history['training'][-1], self.acc_history['validation'][-1]))
             np.savez('{:s}/{:s}_loss_history_{:03d}.npz'.format(self.model_dir, self.model.name, epoch), self.loss_history)
