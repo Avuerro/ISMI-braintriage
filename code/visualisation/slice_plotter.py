8c43420337744b956d49e4d749a3658bf400da00
Chihab <chihabamghane@gmail.com>
2020-06-09 18:53:23 +0200

    removed repeted code snippets and simplified plot code

--- code/visualisation/slice_plotter.py
+++ code/visualisation/slice_plotter.py
@@ -3,57 +3,73 @@ import matplotlib.pyplot as plt
 
 
 def plot_slices(patient,range_of_slices,DATA_DIR,row_col_number):
+    """
+        This functions plots a single slice or all slices in a list or tuple.
 
-    if type(range_of_slices) == tuple:
-        assert range_of_slices[0] < range_of_slices[1]
-        size_of_range = range_of_slices[1] - range_of_slices[0]
-        nr_of_rows = size_of_range // 4 + 1
-        nr_of_cols = 4 # max 4 plots per row 
+        Params: 
+            patient: integer indicating the patient number
+            range_of_slices: integer slice or list or tuple of slices you want to plot. 
+            DATA_DIR : location of the data
+            row_col_number: amount that dictates the amount of colomns and rows.
 
-        subplots_to_delete = (4 - size_of_range%4) - 1 ## subplots of last row that will turn out empty..
-        
+    """
+    if (type(range_of_slices) == tuple)  or (type(range_of_slices) == list) :
+
+        size_of_range, nr_of_rows, nr_of_cols, subplots_to_delete ,slices = plot_slices_helper(range_of_slices,row_col_number) 
+
         fig,axs = plt.subplots(nr_of_rows,nr_of_cols, figsize=(16,16))
-        for index,slice_number in enumerate(range(range_of_slices[0],range_of_slices[1]+1)): #inclusive..
-            X = torch.load(DATA_DIR + '/' + str(patient) + '_' + str(slice_number) + '.pt')
-            if nr_of_rows > 1:
-                axes = axs[ index//4 , index%4 ]
-            else:
-                axes = axs[index]
-            axes.imshow(X.data.numpy()[2,:,:])
-            axes.set_title("Patient {} slice number {}".format(patient,slice_number))
-            
-        # make sure we don't have these empty white subplots
-        for empty_subplot in range(3, 4 - subplots_to_delete - 1, -1):
-            fig.delaxes(axs[nr_of_rows-1][empty_subplot])
-        fig.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=.65,
-                    wspace=0.35)
-    elif type(range_of_slices) == list:
-        size_of_range = len(range_of_slices)
-        nr_of_rows = size_of_range // row_col_number + 1
-        nr_of_cols = row_col_number # max 4 plots per row 
 
-        subplots_to_delete = (row_col_number - size_of_range%row_col_number) ## subplots of last row that will turn out empty..
-        fig,axs = plt.subplots(nr_of_rows,nr_of_cols, figsize=(16,16))
-        for index,slice_number in enumerate(range_of_slices): #inclusive..
+
+        for index,slice_number in enumerate(slices): #inclusive..
             X = torch.load(DATA_DIR + '/' + str(patient) + '_' + str(slice_number) + '.pt')
             if nr_of_rows > 1:
                 axes = axs[ index//row_col_number , index%row_col_number ]
             else:
                 axes = axs[index]
-            axes.imshow(X.data.numpy()[2,:,:])
+            axes.imshow(X.data.numpy()[2,:,:],cmap="gray")
             axes.set_title("Patient {} slice number {}".format(patient,slice_number))
             
         # make sure we don't have these empty white subplots
-        for empty_subplot in range(row_col_number-1, row_col_number - subplots_to_delete - 1, -1):
+        for empty_subplot in range(row_col_number - 1 , row_col_number - subplots_to_delete - 1, -1):
             fig.delaxes(axs[nr_of_rows-1][empty_subplot])
         fig.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=.65,
                     wspace=0.35)
         fig.tight_layout()
     else:
-        slice_number =range_of_slices
+        slice_number = range_of_slices
         X=torch.load(DATA_DIR + '/' + str(patient) + '_' + str(slice_number) + '.pt')
 
         plt.figure()
         plt.imshow(X.data.numpy()[2,:,:]) 
         plt.title("Patient {} slice number {}".format(patient,slice_number))
     plt.show()
+
+
+def plot_slices_helper(range_of_slices,row_col_number):
+
+    """
+        Helper function that takes over general functionality for the plot_slices function
+        Params:
+            range_of_slices: integer slice or list or tuple of slices you want to plot. 
+            row_col_number: amount that dictates the amount of colomns and rows.
+    """
+    
+    if type(range_of_slices) == tuple:
+        assert range_of_slices[0] < range_of_slices[1]
+        size_of_range = range_of_slices[1] - range_of_slices[0]
+        nr_of_rows = size_of_range // row_col_number + 1
+        nr_of_cols = row_col_number # max 4 plots per row 
+
+        subplots_to_delete = (row_col_number - size_of_range%row_col_number) - 1 
+        slices = range(range_of_slices[0],range_of_slices[1]+1)
+    
+    if type(range_of_slices) == list:
+        size_of_range = len(range_of_slices)
+        nr_of_rows = size_of_range // row_col_number + 1
+        nr_of_cols = row_col_number # max 4 plots per row 
+
+        subplots_to_delete = (row_col_number - size_of_range%row_col_number)
+
+        slices = range_of_slices
+    
+    return size_of_range, nr_of_rows, nr_of_cols, subplots_to_delete ,slices
