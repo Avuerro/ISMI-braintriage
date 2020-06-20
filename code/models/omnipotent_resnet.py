95f13f5ab69ac09efacf6862a18bb140220395e3
fbergh <freekbergh@gmail.com>
2020-06-20 11:38:08 +0200

    Added flatten layer

--- code/models/omnipotent_resnet.py
+++ code/models/omnipotent_resnet.py
@@ -12,6 +12,7 @@ class Net(nn.Module):
         self.model = nn.Sequential(*list(model.children())[:-1])
         self.name = name
         # Final layers
+        self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(fc_in, n_features)  
         self.fc2 = nn.Linear(n_features,1)
         #self.sm1 = nn.Softmax(1)
@@ -21,7 +22,8 @@ class Net(nn.Module):
         x = self.model(x)
         print(f'After resnet layers {x.shape}')
         # Turn x into the right shape
-        x = x.view(x.size(0), -1)
+        x = self.flatten(x)
+        # x = x.view(x.size(0), -1)
         print(f'After reshaping {x.shape}')
         # Put output x through our self defined layers
         x = self.fc1(x)
