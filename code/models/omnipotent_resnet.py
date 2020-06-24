fe523b3115e782177dc7384322b3cd89818482bd
Max Driessen <driessenmag@gmail.com>
2020-06-24 19:18:37 +0200

    removed redundant prints

--- code/models/omnipotent_resnet.py
+++ code/models/omnipotent_resnet.py
@@ -18,18 +18,13 @@ class Net(nn.Module):
         #self.sm1 = nn.Softmax(1)
 
     def forward(self, x):
-        print(f'Start resnet {x.shape}')
         x = self.model(x)
-        print(f'After resnet layers {x.shape}')
         # Turn x into the right shape
         x = self.flatten(x)
         # x = x.view(x.size(0), -1)
-        print(f'After reshaping {x.shape}')
         # Put output x through our self defined layers
         x = self.fc1(x)
-        print(f'After first FC layer {x.shape}')
         out = self.fc2(x)
-        print(f'After final FC layer {out.shape}')
         #print(out)
 
         
